2539b8c9d053c210df17be7a48c1cfd60c469f9a
jeyren95 <jeyren95@hotmail.com>
2025-04-17 16:00:08 +0800

    updated styling for conversion table

--- web/src/components/common/table/Table.tsx
+++ web/src/components/common/table/Table.tsx
@@ -1,9 +1,13 @@
 import { type ComponentProps } from "react";
-import { Table as MuiTable } from "@mui/material";
+import { Table as MuiTable, TableContainer as MuiTableContainer } from "@mui/material";
 import clsx from "clsx";
 
 function Table({ children, className }: ComponentProps<typeof MuiTable>) {
-	return <MuiTable className={clsx("table", className)}>{children}</MuiTable>;
+	return (
+		<MuiTableContainer>
+			<MuiTable className={clsx("table", className)}>{children}</MuiTable>
+		</MuiTableContainer>
+	)
 }
 
 export default Table;
