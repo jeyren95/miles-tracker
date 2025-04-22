7297206428a5dd313484c2caf2110afeca1e4476
jeyren95 <jeyren95@hotmail.com>
2025-04-22 21:02:22 +0800

    added trip type switch and replaced progress paper with card

---
+++ web/src/components/ClassProgressCard.tsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import {
+  LinearProgress as MuiLinearProgress,
+  Card as MuiCard,
+  CardHeader as MuiCardHeader,
+  CardContent as MuiCardContent,
+} from "@mui/material";
+
+import { type ClassProgressPaperProps } from "../types/goals";
+import { calculatePercentageProgress } from "../utils";
+import { UserContext } from "../context";
+
+function ClassProgressCard({ description, miles }: ClassProgressPaperProps) {
+  const { user } = useContext(UserContext) || {};
+  const progress = calculatePercentageProgress(user?.totalMiles || 0, miles);
+
+  return (
+    <MuiCard className="goals-page__class-progress-card">
+      <MuiCardHeader
+        title={description}
+        subheader={`Miles required: ${miles}`}
+      />
+      <MuiCardContent className="goals-page__class-progress-card-content">
+        <MuiLinearProgress
+          className="goals-page__progress-bar"
+          variant="determinate"
+          value={progress}
+        />
+        <p>{progress}%</p>
+      </MuiCardContent>
+    </MuiCard>
+  );
+}
+
+export default ClassProgressCard;
--- web/src/components/ProgressDialog.tsx
+++ web/src/components/ProgressDialog.tsx
@@ -1,132 +1,159 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import {
   Dialog as MuiDialog,
   DialogTitle as MuiDialogTitle,
-	DialogContent as MuiDialogContent,
+  DialogContent as MuiDialogContent,
+  Switch as MuiSwitch,
 } from "@mui/material";
 
-import ClassProgressPaper from "./ClassProgressPaper";
+import ClassProgressCard from "./ClassProgressCard";
 
 import {
   type ProgressDialogProps,
   type GetMilesRes,
   TripType,
 } from "../types/goals";
+import { UserContext } from "../context";
 import { useFetchData } from "../hooks/useFetchData";
 
 const DUMMY_DATA = [
-    {
-        "tripType": "One-way",
-        "description": "Economy Saver Award Ticket",
-        "miles": 27000,
-        "classType": "Economy"
-    },
-    {
-        "tripType": "One-way",
-        "description": "Economy Advantage Award Ticket",
-        "miles": 45000,
-        "classType": "Economy"
-    },
-    {
-        "tripType": "One-way",
-        "description": "Premium Economy Saver Award Ticket",
-        "miles": 37500,
-        "classType": "Premium Economy"
-    },
-    {
-        "tripType": "One-way",
-        "description": "Business Saver Award Ticket",
-        "miles": 52000,
-        "classType": "Business"
-    },
-    {
-        "tripType": "One-way",
-        "description": "Business Advantage Award Ticket",
-        "miles": 70000,
-        "classType": "Business"
-    },
-    {
-        "tripType": "One-way",
-        "description": "First Saver Award Ticket",
-        "miles": 77000,
-        "classType": "First"
-    },
-    {
-        "tripType": "One-way",
-        "description": "First Advantage Award Ticket",
-        "miles": 120000,
-        "classType": "First"
-    },
-    {
-        "tripType": "Return",
-        "description": "Economy Saver Award Ticket",
-        "miles": 54000,
-        "classType": "Economy"
-    },
-    {
-        "tripType": "Return",
-        "description": "Economy Advantage Award Ticket",
-        "miles": 90000,
-        "classType": "Economy"
-    },
-    {
-        "tripType": "Return",
-        "description": "Premium Economy Saver Award Ticket",
-        "miles": 75000,
-        "classType": "Premium Economy"
-    },
-    {
-        "tripType": "Return",
-        "description": "Business Saver Award Ticket",
-        "miles": 104000,
-        "classType": "Business"
-    },
-    {
-        "tripType": "Return",
-        "description": "Business Advantage Award Ticket",
-        "miles": 140000,
-        "classType": "Business"
-    },
-    {
-        "tripType": "Return",
-        "description": "First Saver Award Ticket",
-        "miles": 154000,
-        "classType": "First"
-    },
-    {
-        "tripType": "Return",
-        "description": "First Advantage Award Ticket",
-        "miles": 240000,
-        "classType": "First"
-    }
+  {
+    tripType: "One-way",
+    description: "Economy Saver Award Ticket",
+    miles: 27000,
+    classType: "Economy",
+  },
+  {
+    tripType: "One-way",
+    description: "Economy Advantage Award Ticket",
+    miles: 45000,
+    classType: "Economy",
+  },
+  {
+    tripType: "One-way",
+    description: "Premium Economy Saver Award Ticket",
+    miles: 37500,
+    classType: "Premium Economy",
+  },
+  {
+    tripType: "One-way",
+    description: "Business Saver Award Ticket",
+    miles: 52000,
+    classType: "Business",
+  },
+  {
+    tripType: "One-way",
+    description: "Business Advantage Award Ticket",
+    miles: 70000,
+    classType: "Business",
+  },
+  {
+    tripType: "One-way",
+    description: "First Saver Award Ticket",
+    miles: 77000,
+    classType: "First",
+  },
+  {
+    tripType: "One-way",
+    description: "First Advantage Award Ticket",
+    miles: 120000,
+    classType: "First",
+  },
+  {
+    tripType: "Return",
+    description: "Economy Saver Award Ticket",
+    miles: 54000,
+    classType: "Economy",
+  },
+  {
+    tripType: "Return",
+    description: "Economy Advantage Award Ticket",
+    miles: 90000,
+    classType: "Economy",
+  },
+  {
+    tripType: "Return",
+    description: "Premium Economy Saver Award Ticket",
+    miles: 75000,
+    classType: "Premium Economy",
+  },
+  {
+    tripType: "Return",
+    description: "Business Saver Award Ticket",
+    miles: 104000,
+    classType: "Business",
+  },
+  {
+    tripType: "Return",
+    description: "Business Advantage Award Ticket",
+    miles: 140000,
+    classType: "Business",
+  },
+  {
+    tripType: "Return",
+    description: "First Saver Award Ticket",
+    miles: 154000,
+    classType: "First",
+  },
+  {
+    tripType: "Return",
+    description: "First Advantage Award Ticket",
+    miles: 240000,
+    classType: "First",
+  },
 ];
 
-function ProgressDialog({
-  onClose,
-  open,
-  selectedRow,
-}: ProgressDialogProps) {
-	const [selectedTripType, setSelectedTripType] = useState(TripType.RETURN);
+function ProgressDialog({ onClose, open, selectedRow }: ProgressDialogProps) {
+  const { user } = useContext(UserContext) || {};
+  const [selectedTripType, setSelectedTripType] = useState(TripType.RETURN);
   // const { origin, destination } = selectedRow ?? {};
   // const endPoint = `${import.meta.env.VITE_API_ENDPOINT}/miles?origin=${origin}&destination=${destination}`;
   // const { isLoading, error, data } = useFetchData<GetMilesRes>(endPoint);
 
   // if (isLoading) return <div>Loading...</div>;
   // if (error) return <div>{error}</div>;
-	
-	const filteredData = DUMMY_DATA.filter((d) => d.tripType === selectedTripType);
+
+  function handleSwitchChange() {
+    if (selectedTripType === TripType.RETURN) {
+      setSelectedTripType(TripType.ONE_WAY);
+    } else {
+      setSelectedTripType(TripType.RETURN);
+    }
+  }
+
+  const filteredData = DUMMY_DATA.filter(
+    (d) => d.tripType === selectedTripType,
+  );
 
   return (
     <MuiDialog
-			fullWidth
-			maxWidth="xl"
+      fullWidth
+      maxWidth="lg"
       className="goals-page__progress-dialog"
       open={open}
       onClose={onClose}
     >
-      <MuiDialogTitle>Check out your progression here!</MuiDialogTitle>
+      <MuiDialogTitle className="goals-page__progress-dialog-title">
+        <div>
+          You have accumulated{" "}
+          <span className="goals-page__accumulated-miles">
+            {user?.totalMiles}
+          </span>{" "}
+          miles.
+        </div>
+        <div className="goals-page__trip-type-toggle">
+          <span>{TripType.ONE_WAY}</span>
+          <MuiSwitch
+            onChange={handleSwitchChange}
+            checked={selectedTripType === TripType.RETURN}
+          />
+          <span>{TripType.RETURN}</span>
+        </div>
+      </MuiDialogTitle>
       <MuiDialogContent className="goals-page__progress-dialog-content">
-				{filteredData.map((d) => <ClassProgressPaper {...d} />)}
+        {filteredData.map((d) => (
+          <ClassProgressCard {...d} />
+        ))}
       </MuiDialogContent>
     </MuiDialog>
   );
